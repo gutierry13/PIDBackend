6d67476aba1b5798c92abe7ff80b109e3d644e62
Alexandre Gutierry P. Nunes <aleguti1000@hotmail.com>
2023-08-26 19:54:39 -0300

    Update conexao.js

--- Persistencia/conexao.js
+++ Persistencia/conexao.js
@@ -8,7 +8,7 @@ export default async function conectar() {
     host: 'localhost',
     user: 'aluno3-pfsii',
     port: 3306,
-    password: 'aluno3-pfsii',
+    password: 'njTMBR4Y01wcxju01OxV',
     database: 'backend'
   })
   global.conexao = conexao
