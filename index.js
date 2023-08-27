f67480fce66ef97353c03f4d945412eba8ce106c
Alexandre Gutierry <aleguti1000@hotmail.com>
2023-08-27 12:22:18 -0300

    test for production

--- index.js
+++ index.js
@@ -3,14 +3,15 @@ import clienteRota from './Rotas/clienteRota.js'
 import cors from 'cors'
 import animalRota from './Rotas/animalRota.js'
 import adocaoRota from './Rotas/adocaoRota.js'
-const server = express()
-
-server.use(cors())
+const server = new express()
+const hostname = '0.0.0.0';
+const port = 4003
+server.use(cors({origin:"*"}));
 server.use(express.urlencoded({ extended: false }))
 server.use(express.json())
 server.use('/clientes', clienteRota)
 server.use('/animais', animalRota)
 server.use('/adocoes', adocaoRota)
-server.listen(4003, 'localhost', () => {
-  console.log('Backend rodando em http://localhost:4003')
+server.listen(port, hostname, () => {
+  console.log(`Backend rodando em http://${hostname}:${port}`)
 })
