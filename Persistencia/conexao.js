6cd2d74bf2af3247481b17f99abc50252be50187
Alexandre Gutierry P. Nunes <aleguti1000@hotmail.com>
2023-10-19 18:51:50 -0300

    Update conexao.js

--- Persistencia/conexao.js
+++ Persistencia/conexao.js
@@ -5,24 +5,24 @@ export default async function conectar() {
     return await global.poolConexoes.getConnection();
   }
   const poolConexoes = mysql.createPool({
-    host: "localhost",
-    user: "root",
-    password: "",
-    database: "backend",
-    port: 3306,
-    //-- Production
     // host: "localhost",
-    // user: "aluno3-pfsii",
-    // password: "njTMBR4Y01wcxju01OxV",
+    // user: "root",
+    // password: "",
     // database: "backend",
     // port: 3306,
-    // waitForConnections: true,
-    // connectionLimit: 10,
-    // queueLimit: 0,
-    // maxIdle: 10,
-    // idleTimeout: 60000,
-    // enableKeepAlive: true,
-    // keepAliveInitialDelay: 0,
+    //-- Production
+    host: "localhost",
+    user: "aluno3-pfsii",
+    password: "njTMBR4Y01wcxju01OxV",
+    database: "backend",
+    port: 3306,
+    waitForConnections: true,
+    connectionLimit: 10,
+    queueLimit: 0,
+    maxIdle: 10,
+    idleTimeout: 60000,
+    enableKeepAlive: true,
+    keepAliveInitialDelay: 0,
   });
   global.poolConexoes = poolConexoes;
   return await poolConexoes.getConnection();
